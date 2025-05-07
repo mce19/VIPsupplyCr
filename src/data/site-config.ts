a962c5b04f9d32b0c3efe3f51913c77c657f56a2
Maykol Castro <maycolcastroesp19@gmail.com>
2025-05-07 01:15:00 -0600

    cambios estaticos

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -31,12 +31,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Distribuidor Exclusivo de Luminosity en Costa Rica | Productos Capilares de Alta Calidad',
-    subtitle: 'Transforma el Cuidado Capilar con Luminosity – Innovación y Calidad Garantizada por Brazil Cosmetics',
-    description: 'Somos el distribuidor exclusivo de Luminosity en Costa Rica, ofreciendo productos capilares de alta calidad respaldados por Brazil Cosmetics. Nuestro compromiso es proporcionar soluciones seguras y efectivas para salones de belleza, con opciones de financiamiento flexibles y una rentabilidad superior. Descubre productos certificados, con resultados de larga duración, que garantizan un alisado perfecto y un cuidado capilar profesional. Mejora la experiencia de tus clientes con Luminosity y haz crecer tu negocio con un socio confiable.',
+    title: 'Distribuidor Exclusivo de Nanoplastia UNIKA en Costa Rica',
+    subtitle: 'Transforma el Cuidado Capilar con UNIKA tendrás Innovación y Calidad Garantizada por Agilise Cosméticos',
+    description: 'Somos el distribuidor exclusivo de Nanoplastia UNIKA en Costa Rica, ofreciendo productos capilares de alta calidad respaldados por Agilise Cosméticos. Nuestro compromiso es proporcionar soluciones seguras y efectivas para salones de belleza, con opciones de financiamiento flexibles y una rentabilidad superior. Descubre productos certificados, con resultados de larga duración, que garantizan un alisado perfecto y un cuidado capilar profesional. Mejora la experiencia de tus clientes con Nanoplastia UNIKA y haz crecer tu negocio con un socio confiable.',
     image: {
-        src: '/images/Nano Radiant.jpg',
-        alt: 'Distribuidor exclusivo de Luminosity en Costa Rica'
+        src: '/images/UNIKA.jpg',
+        alt: 'Distribuidor exclusivo de Nanoplastia UNIKA en Costa Rica'
     },
   
 };
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,7 @@
 const services = [
     {
         title: "",
-        description: "La calidad de este producto está garantizada por la gran Empresa Brazil Cosmetics.",
+        description: "La calidad de este producto está garantizada por la gran Empresa Agilise Cosméticos.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
@@ -19,7 +19,7 @@ const services = [
     },
     {
         title: "",
-        description: "Producto con registro sanitario en ANVISA (Ministerio de salud brasileño). 25351.508936/2022-49",
+        description: "Producto con registro sanitario en ANVISA (Ministerio de salud brasileño). 25351.367836/2017-34",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
